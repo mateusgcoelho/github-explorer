7d360e1752acd4e9fb47ef2cf82a23ab100a6426
João Mateus G Coelho <mateusgamescoelho547@gmail.com>
2021-07-15 16:15:04 -0300

    Add: Fast Refresh no webpack

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,5 @@ import './styles/global.scss';
 import { RepositoryList } from './components/RepositoryList';
 
 export function App() {
-    return(
-        <>
-            <RepositoryList />
-        </>
-    );
+    return <RepositoryList />;
 }
